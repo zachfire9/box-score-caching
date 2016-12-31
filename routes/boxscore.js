16a376c14c3f82eae503fc00e7b81e8545fce0af
Zach Firestone <zachfire9@gmail.com>
2016-12-31 02:23:00 -0500

    Saving boxscore json in mongo.

--- routes/boxscore.js
+++ routes/boxscore.js
@@ -1,3 +1,4 @@
+const Mongoose = require('mongoose');
 const Wreck = require('wreck');
 
 const Config = require('../config');
@@ -12,7 +13,16 @@ module.exports = [
         path: '/boxscore/{season}/{gameid}', 
         handler: function (request, reply) {
             Wreck.get(Config.get('/feed') + request.params.season + '/game_boxscore.json?gameid=' + request.params.gameid, options, (err, res, payload) => {
-                reply(payload);
+                const schema = Mongoose.Schema({}, { strict: false });
+                const Boxscore = Mongoose.model('Boxscore', schema);
+                const gameBoxscore = new Boxscore(payload);
+                gameBoxscore.save(function (err, result) {
+                    if (err) {
+                        console.error(err);
+                        return reply(err);
+                    }
+                    return reply(true);
+                });
             });
         } 
     }
